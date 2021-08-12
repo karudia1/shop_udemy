532d031b6f67a2646cc9b1d1f6b01e482bbb6157
karudia1 <karudia1@yahoo.com.br>
2021-08-12 09:22:40 -0300

    Criação do projeto que tem como foco gerenciar os estados do app

---
+++ shop/lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: Center(
+        child: Text('Vamos desenvolver uma loja?'),
+      ),
+    );
+  }
+}
